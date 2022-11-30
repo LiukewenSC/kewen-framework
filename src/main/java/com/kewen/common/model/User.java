3074df0194e8673a016fe42a017c64aa36d4dfeb
kewen <LiuKewenSC@outlook.com>
2022-11-30 21:58:46 +0800

    feat:common包初始化

---
+++ src/main/java/com/kewen/common/model/User.java
@@ -0,0 +1,51 @@
+package com.kewen.common.model;
+
+
+import java.util.Objects;
+
+/**
+ * @descrpition 用户
+ * @author kewen
+ * @since 2022-11-23 10:32
+ */
+public class User {
+    private Integer id;
+    private String name;
+
+    public User() {
+    }
+
+    public User(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
