41ef3135fafcdd0c48bd31d9ba3b47c70a659cd9
Kewen <LiuKewenSC@outlook.com>
2022-12-05 15:50:18 +0800

    style: 调整项目结构

---
+++ base/common/src/main/java/com/kewen/common/utils/BeanUtil.java
@@ -0,0 +1,55 @@
+package com.kewen.common.utils;
+
+import com.alibaba.fastjson2.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * @author kewen
+ * @descrpition
+ * @since 2022-11-30 20:31
+ */
+public class BeanUtil {
+
+    /**
+     * 转换成 bean 用fastjson，不用再做字段类型匹配，快于spring和common-lang包
+     * @param source 源
+     * @param clazz 转换的类型
+     * @param <T> 返回泛型
+     * @return
+     */
+    public static <T> T toBean(Object source,Class<T> clazz){
+        String s = JSONObject.toJSONString(source);
+        return JSONObject.parseObject(s, clazz);
+    }
+
+
+    /**
+     * 克隆Bean，复制Bean中的值，深度复制
+     * @param source
+     * @param <T>
+     * @return
+     */
+    public static <T> T copy(T source){
+        String jsonString = com.alibaba.fastjson.JSONObject.toJSONString(source);
+        return (T) com.alibaba.fastjson.JSONObject.parseObject(jsonString, source.getClass());
+    }
+
+    /**
+     * 克隆集合
+     * @param sources
+     * @param <T>
+     * @return
+     */
+    public static <T> List<T> copyList(Collection<T> sources){
+        ArrayList<T> lists = new ArrayList<>(sources.size());
+        for (T source : sources) {
+            lists.add(copy(source));
+        }
+        return lists;
+    }
+
+
+}
---
+++ boot/authority-web-boot-starter/src/main/java/com/kewen/framework/boot/authority/biz/model/resp/MenuRespBase.java
@@ -0,0 +1,66 @@
+package com.kewen.framework.boot.authority.biz.model.resp;
+
+import com.kewen.framework.boot.authority.enums.MenuAuthType;
+import com.kewen.framework.boot.authority.enums.MenuType;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+/**
+ * @descrpition 菜单返回结构
+ * @author kewen
+ * @since 2022-12-01 10:42
+ */
+@Data
+public class MenuRespBase {
+    /**
+     * 主键id
+     */
+    protected Integer id;
+
+    /**
+     * 菜单名
+     */
+    protected String name;
+
+    /**
+     * 父id
+     */
+    protected Integer parentId;
+
+    /**
+     * 链接或路径
+     */
+    protected String url;
+
+    /**
+     * 图片地址
+     */
+    protected String image;
+
+    /**
+     * 类型： 1-菜单 2-按钮 3-外部链接
+     */
+    protected MenuType type;
+    /**
+     * 权限类型 1-基于父菜单权限 2-基于本身权限
+     */
+    protected MenuAuthType authType;
+
+    /**
+     * 描述
+     */
+    protected String description;
+
+    /**
+     * 创建时间
+     */
+    protected LocalDateTime createTime;
+
+    /**
+     * 修改时间
+     */
+    protected LocalDateTime updateTime;
+
+}
+
