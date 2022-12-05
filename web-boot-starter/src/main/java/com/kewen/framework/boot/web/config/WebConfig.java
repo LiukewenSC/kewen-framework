5413773c2751c65d04faf59c8e4bfd59a42fdf34
Kewen <LiuKewenSC@outlook.com>
2022-12-05 10:26:02 +0800

    feat: kewen-framework-boot-web 加入

---
+++ web-boot-starter/src/main/java/com/kewen/framework/boot/web/config/WebConfig.java
@@ -0,0 +1,29 @@
+package com.kewen.framework.boot.web.config;
+
+import com.kewen.framework.boot.web.interceptor.TenantInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @descrpition web配置
+ * @author kewen
+ * @since 2022-12-05 9:36
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    TenantInterceptor tenantInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addWebRequestInterceptor(tenantInterceptor);
+    }
+
+
+
+
+
+}
