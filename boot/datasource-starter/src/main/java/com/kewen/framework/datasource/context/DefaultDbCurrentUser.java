cf41025a309aae87aa7241fcfa61a68f8c006347
Kewen <LiuKewenSC@outlook.com>
2022-12-05 16:33:24 +0800

    feat: DB starter

---
+++ boot/datasource-starter/src/main/java/com/kewen/framework/datasource/context/DefaultDbCurrentUser.java
@@ -0,0 +1,26 @@
+package com.kewen.framework.datasource.context;
+
+
+import com.kewen.common.context.UserContext;
+import com.kewen.common.model.IUser;
+
+/**
+ * 默认用户上下文,保存数据时自动填充createUserId和createUserName
+ *
+ * @author liukewen
+ * @since 2022/9/2
+ */
+public class DefaultDbCurrentUser implements DbCurrentUser {
+
+    @Override
+    public Long getUserId() {
+        IUser iUser = UserContext.get();
+        return iUser == null ? null : iUser.getUserId();
+    }
+
+    @Override
+    public String getUserName() {
+        IUser iUser = UserContext.get();
+        return iUser == null ? null : iUser.getUserName();
+    }
+}
