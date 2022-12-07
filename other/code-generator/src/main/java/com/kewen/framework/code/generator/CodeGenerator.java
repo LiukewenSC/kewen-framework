1e10adcdb3d33348c8cc3a1feef0314d145ad10b
kewen <LiuKewenSC@outlook.com>
2022-12-07 17:08:23 +0800

    feat: 代码生成器修改，加入新配置

--- other/code-generator/src/main/java/com/kewen/framework/code/generator/CodeGenerator.java
+++ other/code-generator/src/main/java/com/kewen/framework/code/generator/CodeGenerator.java
@@ -7,6 +7,8 @@ import com.baomidou.mybatisplus.generator.config.OutputFile;
 import com.baomidou.mybatisplus.generator.config.rules.DateType;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 import com.baomidou.mybatisplus.generator.engine.VelocityTemplateEngine;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.util.CollectionUtils;
 
 import java.io.FileInputStream;
 import java.sql.Connection;
@@ -17,7 +19,9 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
 import java.util.Properties;
 
 /**
@@ -37,29 +41,37 @@ public class CodeGenerator {
         //classpath路径
         String classpath = path.substring(0, path.indexOf("classes") + "classes".length());
         //工程路径
-        String projectPath = path.substring(0, path.indexOf("/target/classes"));
+        String projectPath ;
+        if (StringUtils.isNotBlank(Config.ABSOLUTE_PATH)){
+            projectPath = Config.ABSOLUTE_PATH;
+        } else {
+            projectPath = path.substring(0, path.indexOf("/target/classes"));
+        }
 
 
         //数据源配置
         DataSourceConfig.Builder dataSourceConfigBuilder = getDateSourceConfigBuilder(classpath);
 
-        //getAllTableName(dataSourceConfigBuilder);
+        List<String> tableNames = getTableNames(dataSourceConfigBuilder);
 
         //创建配置
-        FastAutoGenerator autoGenerator = createAutoGenerator(dataSourceConfigBuilder, projectPath);
+        FastAutoGenerator autoGenerator = createAutoGenerator(dataSourceConfigBuilder, projectPath,tableNames);
         //生成文件
         autoGenerator.execute();
     }
 
-    private static FastAutoGenerator createAutoGenerator(DataSourceConfig.Builder dataSourceConfigBuilder, String projectPath) {
+    private static FastAutoGenerator createAutoGenerator(DataSourceConfig.Builder dataSourceConfigBuilder, String projectPath,List<String> tableNames) {
         return FastAutoGenerator.create(dataSourceConfigBuilder)
                 .globalConfig(builder -> {
                     builder.author(Config.AUTHOR)
-                            .enableSwagger()
                             .disableOpenDir()
                             .commentDate(() -> LocalDateTime.now().format(DateTimeFormatter.ISO_DATE))
                             .outputDir(projectPath + "/src/main/java")
-                            .dateType(DateType.TIME_PACK);
+                            .dateType(DateType.TIME_PACK)
+                    ;
+                    if (Config.ENABLE_SWAGGER){
+                        builder.enableSwagger();
+                    }
                 }).packageConfig(builder -> {
                     //包配置
                     builder.parent(Config.BASE_PACKAGE)
@@ -79,41 +91,40 @@ public class CodeGenerator {
                             .addInclude()//添加表匹配，指定要生成的数据表名，不写默认选定数据库所有表
                             //.disableSqlFilter()禁用sql过滤:默认(不使用该方法）true
                             //.enableSchema()启用schema:默认false
-
+                            .addInclude(tableNames)
                             .entityBuilder() //实体策略配置
-                            //.disableSerialVersionUID()禁用生成SerialVersionUID：默认true
-                            .fileOverride()
-                            .enableChainModel()//开启链式模型
-                            .enableActiveRecord() //开启 ActiveRecord 模式
-                            .enableLombok()//开启lombok
-                            //.enableRemoveIsPrefix()//开启 Boolean 类型字段移除 is 前缀
-                            .enableTableFieldAnnotation()//开启生成实体时生成字段注解
-                            //.addTableFills()添加表字段填充
-                            .naming(NamingStrategy.underline_to_camel)//数据表映射实体命名策略：默认下划线转驼峰underline_to_camel
-                            .columnNaming(NamingStrategy.underline_to_camel)//表字段映射实体属性命名规则：默认null，不指定按照naming执行
-                            .idType(IdType.AUTO)//添加全局主键类型
-                            //.formatFileName("%s")//格式化实体名称，%s取消首字母I
-                            .build()
+                                //.disableSerialVersionUID()禁用生成SerialVersionUID：默认true
+                                .fileOverride()
+                                .enableChainModel()//开启链式模型
+                                .enableActiveRecord() //开启 ActiveRecord 模式
+                                .enableLombok()//开启lombok
+                                //.enableRemoveIsPrefix()//开启 Boolean 类型字段移除 is 前缀
+                                .enableTableFieldAnnotation()//开启生成实体时生成字段注解
+                                //.addTableFills()添加表字段填充
+                                .naming(NamingStrategy.underline_to_camel)//数据表映射实体命名策略：默认下划线转驼峰underline_to_camel
+                                .columnNaming(NamingStrategy.underline_to_camel)//表字段映射实体属性命名规则：默认null，不指定按照naming执行
+                                .idType(IdType.AUTO)//添加全局主键类型
+                                //.formatFileName("%s")//格式化实体名称，%s取消首字母I
+                                .build()
                             .mapperBuilder()//mapper文件策略
-                            .fileOverride()
-                            //.enableMapperAnnotation()//开启mapper注解
-                            .enableBaseResultMap()//启用xml文件中的BaseResultMap 生成
-                            .enableBaseColumnList()//启用xml文件中的BaseColumnList
-                            //.cache(缓存类.class)设置缓存实现类
-                            //.formatMapperFileName("%sDao")//格式化Dao类名称
-                            .formatXmlFileName("%sXml")//格式化xml文件名称
-                            .build()
+                                .fileOverride()
+                                //.enableMapperAnnotation()//开启mapper注解
+                                .enableBaseResultMap()//启用xml文件中的BaseResultMap 生成
+                                .enableBaseColumnList()//启用xml文件中的BaseColumnList
+                                //.cache(缓存类.class)设置缓存实现类
+                                //.formatMapperFileName("%sDao")//格式化Dao类名称
+                                .formatXmlFileName("%sXml")//格式化xml文件名称
+                                .build()
                             .serviceBuilder()//service文件策略
-                            .fileOverride()
-                            .formatServiceFileName("%sService")//格式化 service 接口文件名称
-                            .formatServiceImplFileName("%sServiceImpl")//格式化 service 接口文件名称
-                            .build()
-
+                                .fileOverride()
+                                .formatServiceFileName("%sService")//格式化 service 接口文件名称
+                                .formatServiceImplFileName("%sServiceImpl")//格式化 service 接口文件名称
+                                .build()
                             .controllerBuilder()//控制层策略
-                            .fileOverride()
-                            //.enableHyphenStyle()开启驼峰转连字符，默认：false
-                            .enableRestStyle()//开启生成@RestController
-                            .formatFileName("%sController")//格式化文件名称
+                                .fileOverride()
+                                //.enableHyphenStyle()开启驼峰转连字符，默认：false
+                                .enableRestStyle()//开启生成@RestController
+                                .formatFileName("%sController")//格式化文件名称
                     ;
 
                 })
@@ -126,9 +137,10 @@ public class CodeGenerator {
 
                 })
                 .templateEngine(new VelocityTemplateEngine())
-                .injectionConfig(builder -> {
-                    //builder.customMap()
-                });
+                /*.injectionConfig(builder -> {
+                    builder.customMap(Collections.singletonMap("enableSwagger",Config.ENABLE_SWAGGER));
+                })*/
+                ;
     }
 
     /**
@@ -160,15 +172,23 @@ public class CodeGenerator {
      * @return
      * @throws SQLException
      */
-    private static List<String> getAllTableName(DataSourceConfig.Builder dataSourceConfigBuilder) throws SQLException {
-        String tableSchema = "??";
-        ArrayList<String> list = new ArrayList<>();
+    private static List<String> getTableNames(DataSourceConfig.Builder dataSourceConfigBuilder) throws SQLException {
+        String tableSchema = Config.SCHEMA;
+        List<String> tableNames = new ArrayList<>();
         Connection connect = dataSourceConfigBuilder.build().getConn();
         Statement stmt = connect.createStatement();
         ResultSet rs = stmt.executeQuery("select table_name from information_schema.tables where table_schema='" + tableSchema + "'");
         while (rs.next()) {
-            list.add(rs.getString("TABLE_NAME"));
+            tableNames.add(rs.getString("TABLE_NAME"));
         }
-        return list;
+        //匹配需要保留的
+        if (!CollectionUtils.isEmpty(Config.CONTAINS_TABLE_PREFIX)){
+            tableNames.removeIf(tableName -> Config.CONTAINS_TABLE_PREFIX.stream().noneMatch(tableName::startsWith));
+        }
+        //排除在排除列表的
+        if (!CollectionUtils.isEmpty(Config.IGNORE_TABLE_PREFIX)){
+            tableNames.removeIf(tableName ->Config.IGNORE_TABLE_PREFIX.stream().anyMatch(tableName::startsWith));
+        }
+        return tableNames;
     }
 }
