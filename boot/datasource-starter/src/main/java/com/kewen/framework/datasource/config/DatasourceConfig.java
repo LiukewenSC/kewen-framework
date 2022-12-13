856a780f7da9b62288b6aebab9a795b02447c214
Kewen <LiuKewenSC@outlook.com>
2022-12-13 16:25:05 +0800

    feat: 数据库连接相关

--- boot/datasource-starter/src/main/java/com/kewen/framework/datasource/config/DatasourceConfig.java
+++ boot/datasource-starter/src/main/java/com/kewen/framework/datasource/config/DatasourceConfig.java
@@ -1,6 +1,5 @@
 package com.kewen.framework.datasource.config;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
@@ -10,7 +9,7 @@ import org.springframework.context.annotation.PropertySource;
  * @since 2022-12-05 16:09
  */
 @Configuration
-@PropertySource("classpath:application-datasource.properties")
+@PropertySource({"classpath:application-datasource.properties","classpath:application-datasource-${spring.profiles.active}.properties"})
 public class DatasourceConfig {
 
 }
