419c158178daca3e754fdec02a294c1f137dbbaa
Kewen <LiuKewenSC@outlook.com>
2022-12-13 16:11:13 +0800

    feat: 权限相关

--- base/common/src/main/java/com/kewen/framework/base/common/model/UserDetail.java
+++ base/common/src/main/java/com/kewen/framework/base/common/model/UserDetail.java
@@ -1,92 +1,39 @@
 package com.kewen.framework.base.common.model;
 
 
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.util.CollectionUtils;
+
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 
 /**
  * @descrpition 用户详情，包含岗位权限等
  * @author kewen
  * @since 2022-11-25 15:17
  */
+@Builder
+@Data
 public class UserDetail {
     protected User user;
     protected UserDept dept;
+    protected Collection<Position> positions;
     protected Collection<Role> roles;
     protected Collection<Permission> permissions;
-
-    public static UserDetailBuilder builder(){
-        return new UserDetailBuilder();
-    }
-
-    public Collection<Dept> getDepts(){
+    public List<Dept> getDepts(){
         if (dept==null) {
             return Collections.emptyList();
         }
-        return dept.getDepts();
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public UserDept getDept() {
-        return dept;
-    }
-
-    public void setDept(UserDept dept) {
-        this.dept = dept;
-    }
-
-    public Collection<Role> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Collection<Role> roles) {
-        this.roles = roles;
-    }
-
-    public Collection<Permission> getPermissions() {
-        return permissions;
-    }
-
-    public void setPermissions(Collection<Permission> permissions) {
-        this.permissions = permissions;
-    }
-
-    public static final class UserDetailBuilder {
-        private UserDetail userDetail;
-
-        private UserDetailBuilder() {
-            userDetail = new UserDetail();
+        Dept primary = dept.getPrimary();
+        Collection<Dept> extras = dept.getExtras();
+        ArrayList<Dept> depts = new ArrayList<>();
+        depts.add(primary);
+        if (!CollectionUtils.isEmpty(extras)){
+            depts.addAll(extras);
         }
-
-        public UserDetailBuilder user(User user) {
-            userDetail.setUser(user);
-            return this;
-        }
-
-        public UserDetailBuilder dept(UserDept dept) {
-            userDetail.setDept(dept);
-            return this;
-        }
-
-        public UserDetailBuilder roles(Collection<Role> roles) {
-            userDetail.setRoles(roles);
-            return this;
-        }
-
-        public UserDetailBuilder permissions(Collection<Permission> permissions) {
-            userDetail.setPermissions(permissions);
-            return this;
-        }
-
-        public UserDetail build() {
-            return userDetail;
-        }
+        return depts;
     }
 }
