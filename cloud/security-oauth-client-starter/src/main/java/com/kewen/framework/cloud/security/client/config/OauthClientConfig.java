bd0e3a3d32355d5112a044b3a7b3703fa1857bb1
kewen <LiuKewenSC@outlook.com>
2022-12-16 22:22:03 +0800

    feat:单点登录搭建，抽象不出来  starter，原因  @EnableSSO和@PropertySource搭配始终有问题，搞不懂

--- cloud/security-oauth-client-starter/src/main/java/com/kewen/framework/cloud/security/client/config/OauthClientConfig.java
+++ cloud/security-oauth-client-starter/src/main/java/com/kewen/framework/cloud/security/client/config/OauthClientConfig.java
@@ -1,6 +1,7 @@
 package com.kewen.framework.cloud.security.client.config;
 
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
@@ -10,8 +11,8 @@ import org.springframework.context.annotation.PropertySource;
  * @since 2022-12-16 17:42
  */
 @Configuration
-@EnableOAuth2Sso
-@PropertySource("classpath:application-security-client.properties")
+@PropertySource(name = "oAuthClientConfigPropertySource",value = "classpath:application-securityclient.properties")
+
 public class OauthClientConfig {
 
 }
