418ac109b1ebc5528f6eb97d7e222c6f6ee50711
Kewen <LiuKewenSC@outlook.com>
2023-01-12 17:39:31 +0800

    feat:修改ConfigurationClassParser的默认xml解析器

---
+++ base/common/src/main/java/com/kewen/framework/base/common/model/SpringConstant.java
@@ -0,0 +1,8 @@
+package com.kewen.framework.base.common.model;
+
+public interface SpringConstant {
+    /**
+     * ConfigurationClassParser 常量，修改ConfigurationClassParser 默认 PropertySourceFactory用到
+     */
+    String configurationClassParserName = "org.springframework.context.annotation.ConfigurationClassParser";
+}
--- boot/web-boot-starter/src/main/java/com/kewen/framework/boot/web/listener/WebSpringApplicationRunListener.java
+++ boot/web-boot-starter/src/main/java/com/kewen/framework/boot/web/listener/WebSpringApplicationRunListener.java
@@ -1,9 +1,20 @@
 package com.kewen.framework.boot.web.listener;
 
+import com.kewen.framework.base.common.factory.YmlPropertySourceFactory;
+import com.kewen.framework.base.common.model.SpringConstant;
+import com.kewen.framework.base.common.utils.BeanUtil;
+import org.springframework.beans.factory.parsing.ProblemReporter;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.BeanNameGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringApplicationRunListener;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.type.classreading.MetadataReaderFactory;
+
+import java.lang.reflect.Constructor;
 
 /**
  * @descrpition 
@@ -22,8 +33,29 @@ public class WebSpringApplicationRunListener implements SpringApplicationRunList
 
     @Override
     public void starting() {
+        //修改ConfigurationClassParser的默认xml解析器
+        modifyConfigurationClassParser();
         System.out.println("starting");
     }
+
+    /**
+     * 修改ConfigurationClassParser的默认xml解析器
+     */
+    private void modifyConfigurationClassParser(){
+        try {
+            Class<?> aClass = Class.forName(SpringConstant.configurationClassParserName);
+            Constructor<?> constructor = aClass.getDeclaredConstructor(
+                    MetadataReaderFactory.class, ProblemReporter.class, Environment.class,
+                    ResourceLoader.class, BeanNameGenerator.class, BeanDefinitionRegistry.class
+            );
+            constructor.setAccessible(true);
+            Object configurationClassParser = constructor.newInstance(null, null, null, null, null, null);
+            BeanUtil.setFinalField(configurationClassParser,"DEFAULT_PROPERTY_SOURCE_FACTORY",new YmlPropertySourceFactory());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
     @Override
     public void environmentPrepared(ConfigurableEnvironment environment) {
