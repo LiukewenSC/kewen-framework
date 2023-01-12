68873a657064d7e24bd76241ae5c4922c4e49548
Kewen <LiuKewenSC@outlook.com>
2023-01-12 16:09:50 +0800

    feat: 测试Spring扩展

---
+++ sample/boot-sample/src/main/java/com/kewen/framework/sample/listener/ExceptionReporter.java
@@ -0,0 +1,22 @@
+package com.kewen.framework.sample.listener;
+
+import org.springframework.boot.SpringBootExceptionReporter;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.util.Assert;
+
+public class ExceptionReporter implements SpringBootExceptionReporter {
+
+
+    ExceptionReporter(ConfigurableApplicationContext context) {
+        this(context, null);
+    }
+
+    ExceptionReporter(ConfigurableApplicationContext context, ClassLoader classLoader) {
+    }
+
+    @Override
+    public boolean reportException(Throwable failure) {
+
+        return false;
+    }
+}
