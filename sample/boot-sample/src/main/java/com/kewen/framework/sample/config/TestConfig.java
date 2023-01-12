2515b4c21840c5fb27e60872dc20dc6162dc9e5f
Kewen <LiuKewenSC@outlook.com>
2023-01-12 16:09:50 +0800

    feat: 测试Spring扩展

---
+++ sample/boot-sample/src/main/java/com/kewen/framework/sample/config/TestConfig.java
@@ -0,0 +1,17 @@
+package com.kewen.framework.sample.config;
+
+import com.kewen.framework.sample.service.TestService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TestConfig {
+
+
+    @Bean
+    public TestService test4Service(){
+        TestService service = new TestService();
+        return service;
+    }
+
+}
