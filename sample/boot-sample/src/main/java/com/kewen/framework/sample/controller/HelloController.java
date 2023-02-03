281e06fd096d3d36c96217d4f03ee4add27d0f17
Kewen <LiuKewenSC@outlook.com>
2023-02-03 09:08:24 +0800

    feat: HelloController

---
+++ sample/boot-sample/src/main/java/com/kewen/framework/sample/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.kewen.framework.sample.controller;
+
+import com.kewen.framework.base.common.model.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Autowired
+    Test2Service test2Service;
+
+    @RequestMapping("/uucs/hello")
+    public Result hello(){
+        return Result.success(test2Service.hello());
+    }
+}
