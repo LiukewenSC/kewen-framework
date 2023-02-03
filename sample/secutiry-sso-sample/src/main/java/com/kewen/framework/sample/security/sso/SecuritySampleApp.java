97c3dac285416253703475ab77c5195761af4908
Kewen <LiuKewenSC@outlook.com>
2023-02-03 14:34:39 +0800

    fix: 将@EnableOauth2SSO依赖进starter中

--- sample/secutiry-sso-sample/src/main/java/com/kewen/framework/sample/security/sso/SecuritySampleApp.java
+++ sample/secutiry-sso-sample/src/main/java/com/kewen/framework/sample/security/sso/SecuritySampleApp.java
@@ -1,5 +1,6 @@
 package com.kewen.framework.sample.security.sso;
 
+import com.kewen.framework.cloud.security.client.config.OauthClientConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
@@ -12,10 +13,10 @@ import org.springframework.context.annotation.ComponentScan;
  * @since 2022-12-16 17:52
  */
 @SpringBootApplication
-@EnableOAuth2Sso
 //@ComponentScan("com.kewen")
 public class SecuritySampleApp {
     public static void main(String[] args) {
-        ConfigurableApplicationContext run = SpringApplication.run(SecuritySampleApp.class, args);
+        //启动类必须引用到配置@EnableOAuth2Sso的，可能跟springboot的启动顺序有关
+        SpringApplication.run(new Class[]{SecuritySampleApp.class, OauthClientConfig.class}, args);
     }
 }
