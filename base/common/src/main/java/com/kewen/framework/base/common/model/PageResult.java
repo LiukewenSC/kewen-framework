5c764b88af483a32f39161a1ddf2a4565f8db500
Kewen <LiuKewenSC@outlook.com>
2023-03-30 23:35:11 +0800

    feat: 分页工具

--- base/common/src/main/java/com/kewen/framework/base/common/model/PageResult.java
+++ base/common/src/main/java/com/kewen/framework/base/common/model/PageResult.java
@@ -2,13 +2,15 @@ package com.kewen.framework.base.common.model;
 
 import lombok.Data;
 
+import java.util.List;
+
 /**
  * @author kewen
  * @descrpition 分页返回
  * @since 2023-03-29
  */
 @Data
-public class PageResult {
+public class PageResult<T> {
     /**
      * 页码
      */
@@ -19,4 +21,18 @@ public class PageResult {
     protected Integer size;
 
     protected Integer total;
+
+    protected List<T> records;
+
+    private PageResult() {
+    }
+
+    public static <T> PageResult<T> of(Integer page, Integer size, Integer total, List<T> records){
+        PageResult<T> result = new PageResult<>();
+        result.setPage(page);
+        result.setSize(size);
+        result.setTotal(total);
+        result.setRecords(records);
+        return result;
+    }
 }
