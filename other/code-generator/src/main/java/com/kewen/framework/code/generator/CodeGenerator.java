db6ab200ec33165b270c9460b24546e09154a47e
Kewen <LiuKewenSC@outlook.com>
2023-04-03 22:42:42 +0800

    fix: 修复数据库连接，代码生成，全局异常

--- other/code-generator/src/main/java/com/kewen/framework/code/generator/CodeGenerator.java
+++ other/code-generator/src/main/java/com/kewen/framework/code/generator/CodeGenerator.java
@@ -25,11 +25,10 @@ import java.sql.Statement;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Properties;
 
 /**
@@ -38,6 +37,46 @@ import java.util.Properties;
  * @since 2022-12-05 10:55
  */
 public class CodeGenerator {
+    /**
+     * @descrpition 配置类，代码生成的时候需要配置的信息
+     * @author kewen
+     * @since 2022-12-05 11:05
+     */
+    public static class Config {
+
+        /**
+         * 工程绝对路径地址，指定到src上面一层;
+         * 为空则表示用代码生成器在的路径
+         */
+        public static final String ABSOLUTE_PATH="D:\\Projects\\kewen-blog-backend\\blog-service";
+
+        /**
+         * 基础包名
+         */
+        public static final String BASE_PACKAGE = "com.kewen.blog.service";
+        /**
+         * 模块名
+         */
+        public static final String MODULE_NAME = null;
+
+        /**
+         * 数据库配置文件，resources下的配置文件，主要解析 spring.datasource 下的数据库连接参数
+         */
+        public static final String APPLICATION_CONFIG_FILE = "application-dev";
+        /**
+         * 是否开启swagger
+         */
+        public static final boolean ENABLE_SWAGGER = false;
+        public static final List<String> IGNORE_TABLE_PREFIX = Arrays.asList("bak_","blog_spider","blog_category");
+        //public static final List<String> CONTAINS_TABLE_PREFIX = Arrays.asList("sys_");
+        public static final List<String> CONTAINS_TABLE_PREFIX =Arrays.asList("blog_tag");
+
+        /**
+         * 作者
+         */
+        public static final String AUTHOR = "kewen";
+
+    }
 
     public static void main(String[] args) throws SQLException {
 
@@ -217,4 +256,6 @@ public class CodeGenerator {
         }
         return tableNames;
     }
+
+
 }
