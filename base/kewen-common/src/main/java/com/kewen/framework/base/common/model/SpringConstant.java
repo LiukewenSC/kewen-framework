bd8ac82ba6340dc3357484320ebf517a1957ea30
Kewen <LiuKewenSC@outlook.com>
2023-04-07 20:55:32 +0800

    refactor: SpringConstant移动， -> ConfigurationClassParser 常量，修改ConfigurationClassParser 默认 PropertySourceFactory用到

--- base/kewen-common/src/main/java/com/kewen/framework/base/common/model/SpringConstant.java
+++
@@ -1,8 +0,0 @@
-package com.kewen.framework.base.common.model;
-
-public interface SpringConstant {
-    /**
-     * ConfigurationClassParser 常量，修改ConfigurationClassParser 默认 PropertySourceFactory用到
-     */
-    String configurationClassParserName = "org.springframework.context.annotation.ConfigurationClassParser";
-}
