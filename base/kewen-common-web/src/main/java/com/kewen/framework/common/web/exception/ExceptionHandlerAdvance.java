9f1576389cbf4e722df6da602c4ac562d751e1f6
Kewen <LiuKewenSC@outlook.com>
2023-04-26 22:52:32 +0800

    fix: 修复 security 排除不走过滤器的情况，修复异常校验返回格式问题

--- base/kewen-common-web/src/main/java/com/kewen/framework/common/web/exception/ExceptionHandlerAdvance.java
+++ base/kewen-common-web/src/main/java/com/kewen/framework/common/web/exception/ExceptionHandlerAdvance.java
@@ -3,9 +3,14 @@ package com.kewen.framework.common.web.exception;
 import com.kewen.framework.common.core.exception.BizException;
 import com.kewen.framework.common.core.model.Result;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 /**
  * @descrpition web拦截器
@@ -17,17 +22,31 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ExceptionHandlerAdvance {
 
     @ExceptionHandler(BizException.class)
-    @ResponseStatus
     public Result<Void> bizException(BizException e){
         log.error("bizException >> ",e);
-        return Result.failed(e.getMessage());
+        return Result.failed(Result.BIZ_FAILED,e.getMessage());
     }
-    @ExceptionHandler(Exception.class)
-    @ResponseStatus
-    public Result<Void> exception(Exception e){
-        log.error("exception >> ",e);
-        return Result.failed(e.getMessage());
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Result methodArgumentNotValidException(MethodArgumentNotValidException e){
+
+        BindingResult bindingResult = e.getBindingResult();
+
+        StringBuilder builder = new StringBuilder();
+
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        for (FieldError fieldError : fieldErrors) {
+            String fieldName = fieldError.getField();
+            String message = fieldError.getDefaultMessage();
+            builder.append("[").append(fieldName).append("]").append(message).append(";");
+        }
+        //去掉最后一个;
+        String message = builder.substring(0, builder.length() - 2);
+
+
+        return Result.failed(Result.REQUEST_REJECT,message);
     }
+
     @ExceptionHandler(Throwable.class)
     @ResponseStatus
     public Result<Void> throwable(Throwable e){
