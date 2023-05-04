1f4ac7e0597c3afb51e3ad7a5c48f0828c7ddaab
Kewen <LiuKewenSC@outlook.com>
2023-05-04 22:31:10 +0800

    fix: 增加常量，也没干嘛

---
+++ base/kewen-common-core/src/main/java/com/kewen/framework/common/core/model/CommonConstant.java
@@ -0,0 +1,29 @@
+package com.kewen.framework.common.core.model;
+
+/**
+ * 常量定义
+ *
+ * @author kewen
+ * @since 2023-05-04
+ */
+public interface CommonConstant {
+    /**
+     * 启用/开启
+     */
+    Integer STATUS_OPEN=1;
+    /**
+     * 禁用/关闭
+     */
+    Integer STATUS_CLOSE=0;
+
+
+    /**
+     * 有效
+     */
+    Integer VALID=1;
+    /**
+     * 无效
+     */
+    Integer INVALID=0;
+
+}
