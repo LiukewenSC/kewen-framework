7ce957e99cd203db9b5200be67bee4f949b4ba86
kewen <liukewensc@outlook.com>
2024-10-31 14:51:08 +0800

    fix: 调整名字

--- boot/basic-spring-boot-starter/src/main/java/com/kewen/framework/boot/basic/config/RequestResponseFormatConfig.java
+++ boot/basic-spring-boot-starter/src/main/java/com/kewen/framework/boot/basic/config/RequestResponseFormatConfig.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
-import com.kewen.framework.basic.handler.ExceptionHandlerAdvice;
+import com.kewen.framework.basic.exception.ExceptionHandlerAdvice;
 import com.kewen.framework.basic.logger.trace.TraceResponseBodyAdvice;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
