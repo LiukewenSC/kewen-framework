66920f64b119151bceb0872e3883bf9ab59e4339
kewen <liukewen.liukewen@alibaba-inc.com>
2026-02-26 06:50:26 +0800

    oidc加入配置，待测试

--- qy-idaas/idaas-authentications/src/main/java/com/kewen/framework/idaas/oidc/OidcConfig.java
+++ qy-idaas/idaas-authentications/src/main/java/com/kewen/framework/idaas/oidc/OidcConfig.java
@@ -1,7 +1,11 @@
 package com.kewen.framework.idaas.oidc;
 
 import com.kewen.framework.auth.security.config.HttpSecurityCustomizer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsByNameServiceWrapper;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 /**
  * 2026/02/22
@@ -9,14 +13,21 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
  * @author kewen
  * @since 1.0
  */
+@Configuration
 public class OidcConfig implements HttpSecurityCustomizer {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Override
     public void customizer(HttpSecurity http) throws Exception {
         http
                 .openidLogin(openidLogin -> {
                     openidLogin
                             .loginPage("/login")
-                            .authenticationUserDetailsService(new OidcUserDetailsService());
+                            .authenticationUserDetailsService(
+                                    new UserDetailsByNameServiceWrapper<>(userDetailsService)
+                            );
                 });
     }
 }
